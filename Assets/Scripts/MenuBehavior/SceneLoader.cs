e978664e4748f6aacb91019fac44e1933b9fc007
Yellowthecolor <robertodr1998@gmail.com>
2024-04-30 23:48:27 -0500

    Added TTS disable button, and fixed menu button to only quit

--- Assets/Scripts/MenuBehavior/SceneLoader.cs
+++ Assets/Scripts/MenuBehavior/SceneLoader.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuSettings : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     // Quit Game
-    public void Quit()
+    public void QuitGame()
     {
         Application.Quit();
     }
